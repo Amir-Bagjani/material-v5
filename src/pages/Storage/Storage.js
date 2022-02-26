762ca95db8e7ff0cdd7b9572d1c992ce92f56f0a
amirbagjni <amirbagjani@gmail.com>
2022-02-27 02:25:13 +0330

    toast add

--- src/pages/Storage/Storage.js
+++ src/pages/Storage/Storage.js
@@ -1,12 +1,34 @@
-import Grid from '@mui/material/Grid';
-const Storage = () => {
+import { useState } from "react";
+import BasicToast from "../../components/common/BasicToast";
+import CommonButton from "../../components/common/CommonButton";
+import GridWrapper from "../../components/common/GridWrapper";
 
-    
-    return (
-        <Grid item xs={8}>
-            This is storage page.
-        </Grid>
-    )
-}
+const Storage = () => {
+  const [open, setOpen] = useState(false);
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = (event, reason) => {
+    if (reason === "clickaway") return;
+    setOpen(false);
+  };
+
+  return (
+    <GridWrapper>
+      This is storage page.
+      <CommonButton variant="contained" onClick={handleOpen}>
+        Open success snackbar
+      </CommonButton>
+      <BasicToast
+        open={open}
+        onClose={handleClose}
+        severity="success"
+        message="Yooo World!"
+      />
+    </GridWrapper>
+  );
+};
 
 export default Storage;
